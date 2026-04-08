080d71b19f73d2550f2d2793eba52ee5e7989977
ashishmaruvada <ashishmaruvada@gmail.com>
2026-04-08 16:12:15 +0530

    Add files via upload

---
+++ evenodd.c
@@ -0,0 +1,15 @@
+int main() {
+    int a;
+    printf("enter the value : \n");
+    scanf("%d",&a);
+    if(a%2==0)
+    {
+        printf("the number is even",a);
+        }
+    else
+    {
+        printf("the given number is odd",a);
+    }
+
+return 0;
+}
