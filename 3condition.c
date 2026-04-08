5304928aaa5c9d0c6053eaf8cc2d231a1e77fdc5
ashishmaruvada <ashishmaruvada@gmail.com>
2026-04-08 13:47:57 +0530

    Add files via upload

---
+++ 3condition.c
@@ -0,0 +1,19 @@
+int main() {
+    int a;
+    printf("enter the value : \n");
+    scanf("%d",&a);
+    if(a==0)
+    {
+        printf("the number is equal to zero",a);
+        }
+    else if(a>=0)
+    {
+        printf("the number is posative",a);
+    }
+    else
+    {
+        printf("the given number is negative",a);
+    }
+
+return 0;
+}
