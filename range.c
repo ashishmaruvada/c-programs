217a466c78fcc9a8b9260b51e8403638bc3dbda6
ashishmaruvada <ashishmaruvada@gmail.com>
2026-04-09 09:55:17 +0530

    Add files via upload

---
+++ range.c
@@ -0,0 +1,9 @@
+int main() {
+    int a;
+    printf("enter the value:\n");
+    scanf("%d",&a);
+    int result=(a>=3)&&(a<=13);
+    printf("the %d is in range of 3-13 is %d",a,result);
+
+    return 0;
+}
