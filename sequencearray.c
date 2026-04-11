d1d6184d854f89c106a33e700dbecfb6eb28e895
ashishmaruvada <ashishmaruvada@gmail.com>
2026-04-11 09:48:56 +0530

    Add files via upload

---
+++ sequencearray.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main()
+{
+    int size;
+    printf("enter the values:\n");
+    scanf("%d",&size);
+    int a[size];
+    for(int i=0;i<=size-1;i++)
+    {
+        printf("enter the %d values :\n",i+1);
+        scanf("%d",&a[i]);
+    }
+    for(int i=0;i<=size-1;i++)
+    {
+      
+        if(i!=size-1 && a[i]+1==a[i+1])
+        {
+            printf("%d",a[i]);
+        }
+        else if(i!=0 && a[i]-1==a[i-1])
+        {
+            printf("%d \n",a[i]);
+        }
+    }
+}    
